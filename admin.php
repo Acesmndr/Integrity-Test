8c2f63083a4a1ac9bdf031bea6c5aff7b00fcd92
aayushrijal <reizal.ayz@gmail.com>
2015-01-05 13:52:35 +0545

    all new added

---
+++ admin.php
@@ -0,0 +1,54 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Integrity Test</title>
+<meta charset='UTF-8' />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link rel="stylesheet" type="text/css" href="css/main.css">
+<link rel="stylesheet" type="text/css" href="bootstrap/dist/css/bootstrap.css">
+<!--link rel="stylesheet" type="text/css" href="bootstrap/dist/css/bootstrap.css.map">
+<link rel="stylesheet" type="text/css" href="bootstrap/dist/css/bootstrap-theme.min.css"-->
+<body>
+	<div id="header">
+		<div class="nav">
+			<img src="images/logo.png" id="logo" class="col-md-4">
+				<div class="col-xs-onset-1 signoff">Sign off</div>
+		</div>
+	</div>
+	<div class="container">
+		<!--div class="inputhere">
+			<div class="row">
+				<div class="col-md-3">
+					<div class="options">
+
+					</div>
+				</div>
+			</div>
+		</div-->
+		<div class="editbyAdmin">
+			<div class="table-responsive">
+				<table class="table table-hover">
+					<tr class="active">
+						<th scope="col">ID</th>
+						<th scope="col">Questions</th>
+						<th scope="col">Edit</th>
+						<th scope="col">Delete</th>
+					</tr>
+					<tr class="active">
+						<td>1</td>
+						<td>What is your name?</td>
+						<td><button type="button" class="btn btn-primary" id="">Edit</button></td>
+						<td><button type="button" class="btn btn-danger" id="">Delete</button></td>
+					</tr>
+				</table>
+			</div>
+			<div class="row">
+				<div class="col-xs-1">
+				<button type="button" class="btn btn-primary">New Question</button>
+				</div>
+			</div>
+		</div>
+	</div>
+	<script src="bootstrap/dist/bootstrap.min.js"></script>
+</body>
+</html>
---
+++ login.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Integrity Test</title>
+<meta charset='UTF-8' />
+<meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1">
+<link rel="stylesheet" type="text/css" href="css/main.css">
+<link rel="stylesheet" type="text/css" href="bootstrap/dist/css/bootstrap.css">
+<!--link rel="stylesheet" type="text/css" href="bootstrap/dist/css/bootstrap.css.map">
+<link rel="stylesheet" type="text/css" href="bootstrap/dist/css/bootstrap-theme.min.css"-->
+<body>
+	<div id="header">
+		<div class="nav">
+			<img src="images/logo.png" id="logo" class="col-md-4">
+		</div>
+	</div>
+	<div class="container">
+		<div class="row">
+			<div class="col-md-5 adminform">
+				<div class="welcomeAdmin">Hello Admin, Please Log In</div>
+				<form>
+ 					 <div class="form-group">
+  						<label for="username">Username</label>
+ 						<input type="email" class="form-control" id="exampleInputEmail1" placeholder="Username">
+					</div>
+ 					<div class="form-group">
+  						<label for="exampleInputPassword1">Password</label>
+    					<input type="password" class="form-control" id="exampleInputPassword1" placeholder="Password">
+  					</div>
+  						<button type="submit" class="btn btn-primary">Log In</button>
+				</form>
+			</div>
+		</div>
+	</div>
+	<script src="bootstrap/dist/bootstrap.min.js"></script>
+</body>
+</html>
