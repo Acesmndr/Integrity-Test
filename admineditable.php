3b5a590d90586bab55726b57544224f50fabf050
acesmndr <acesmndr@gmail.com>
2015-01-07 23:50:01 +0545

    PHP echo disabled

--- admineditable.php
+++ admineditable.php
@@ -10,12 +10,15 @@
 <!--link rel="stylesheet" type="text/css" href="bootstrap/dist/css/bootstrap.css.map">
 <link rel="stylesheet" type="text/css" href="bootstrap/dist/css/bootstrap-theme.min.css"-->
 	<?php
+		ob_start();
 		if($_GET["qid"]){
 			/*for javascript*/
 			//mention code here to get all the data previously stored content from the qid received	
-			include("output_json.php");
-		//test	echo '<script type="text/javascript">alert('.$_GET["qid"].')</script>';
+			//test	echo '<script type="text/javascript">alert('.$_GET["qid"].')</script>';
+			 include("output_json.php");
 		}
+		ob_end_clean();
+		echo '<script type="text/javascript">var phpData='.$json_output.';</script>';
 	?>
 <body>
 	<div id="header">
