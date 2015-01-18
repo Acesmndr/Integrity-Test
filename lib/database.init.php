5428ce5d25509ff4b0c5ec3451aae15c43cfcd03
shresthaankit7 <shresthaankit7@gmail.com>
2015-01-18 20:59:26 +0545

    Password changed..fill according to host

--- lib/database.init.php
+++ lib/database.init.php
@@ -1,6 +1,6 @@
 <?php
 	$DB_host = "localhost";
 	$DB_user = "root";
-	$DB_password = "aces"; // password to be changed in each localhost
+	$DB_password = "xxxxxxxx"; // password to be changed in each localhost
 	$DB_name = "Integrity";
 ?>
